ed2f2f3649326bce4a1eb25d6ba8199c9d2866d2
danthena <30965408+danthena@users.noreply.github.com>
2023-09-21 00:04:21 -0400

    Add files via upload

---
+++ DockerSetup/DockerGroupAddUser.sh
@@ -0,0 +1,2 @@
+# Post Install to add a local user to Docker Group for QOL
+sudo usermod -aG docker $USER
---
+++ DockerSetup/PortainerInstallOnDockerStandaloneLinux.sh
@@ -0,0 +1,11 @@
+# Portainer Deployment on standalone Docker in Linux
+
+# Create Docker Volume for Portainer
+docker volume create portainer_data
+
+# Setting Up Portainer
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data portainer/portainer-ce:latest
+
+# Log into Portainer at https://IPAddress:9443
